0c7f6762e2ada77bcb4349981428cc34ed73a16e
Brett Christensen <brett.christensen@rea-group.com>
2024-01-25 18:29:52 +1100

    create plugin for ha autoconfig and update sg01hp3, mqtt and sensor classes to work with it

--- plugins/deye_plugin_ha-autoconfig.py
+++ plugins/deye_plugin_ha-autoconfig.py
@@ -15,34 +15,57 @@
 # specific language governing permissions and limitations
 # under the License.
 import json
+import logging
+import threading
+import time
+from typing import List, Tuple
 from deye_config import DeyeConfig
-from deye_mqtt import DeyeMqttClient, DeyeMqttPublishError
+from deye_mqtt import DeyeMqttClient
 from deye_plugin_loader import DeyePluginContext
-from deye_events import DeyeEventProcessor, DeyeEvent, DeyeObservationEvent
+from deye_events import DeyeEventList, DeyeEventProcessor
 
-from deye_sensor import SingleRegisterSensor
+from deye_sensor import Sensor
 from deye_sensors import sensor_list
 
     
-class MqttAutoConfPublisher(DeyeMqttClient):
-    def __init__(self, config: DeyeConfig):
-        super().__init__(config)
+class MqttAutoConfPublisher(DeyeEventProcessor):
+    def __init__(self, config: DeyeConfig, mqtt_client: DeyeMqttClient, sensor_configs: List[Tuple[str, str]]):
+        self.__log = logging.getLogger(MqttAutoConfPublisher.__name__)
+        self.__mqtt_client = mqtt_client
+        self.config = config.logger
+        self.sensor_configs = sensor_configs
+        self.publish_sensors_periodically()
+        # Set a timer to call this function again after 4 hours
+        threading.Timer(4 * 60 * 60, self.publish_sensors_periodically).start()
 
-    def __do_publish(self, mqtt_topic: str, value: str):
-        try:
-            info = self._mqtt_client.publish(mqtt_topic, value, qos=1)
-            info.wait_for_publish(1)
-        except ValueError as e:
-            raise DeyeMqttPublishError(f"MQTT outgoing queue is full: {str(e)}")
-        except RuntimeError as e:
-            raise DeyeMqttPublishError(f"Unknown MQTT publishing error: {str(e)}")
-        except OSError as e:
-            raise DeyeMqttPublishError(f"MQTT connection error: {str(e)}")
+
+    def get_id(self):
+        return "mqtt_autoconf_publisher"
+    
+
+    def process(self, events: DeyeEventList):
+        pass
+
+
+    def publish_sensors_periodically(self):
+        # Publish all sensors
+        self.__log.info("(Re)sending autoconf for all sensors")
+        call_again_in_seconds = 4 * 60 * 60
+        # log the date and time this will next be called 
+        next_call_date_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(time.time() + call_again_in_seconds))
+        self.__log.info("Next call to publish_sensors_periodically will be at %s", next_call_date_time)
+        for sensor_config in self.sensor_configs:
+            self.publish_sensor(sensor_config[0], sensor_config[1])
+        
+        t = threading.Timer(call_again_in_seconds, self.publish_sensors_periodically)
+        t.daemon = True
+        t.start()
+
+
 
     def publish_sensor(self, topic: str, value: str):
-        self.__do_publish(topic, value)
-        #self._log.info("Logger is %s", value)
-
+        self.__mqtt_client.do_publish(topic, value)
+        self.__log.info("Logger is %s", value)
 
 
 class DeyePlugin:
@@ -56,36 +79,35 @@ class DeyePlugin:
         Args:
             plugin_context (DeyePluginContext): provides access to core service components, e.g. config
         """
-        
-
         self.config = plugin_context.config
-        self.sensors = sensor_list
-        self.mqtt_client = MqttAutoConfPublisher(plugin_context.config)
-        self.mqtt_client.connect()
-        for sensor in self.sensors:
-            self.__configure_sensors(sensor)
-        self.mqtt_client.disconnect()
+        self.sensors = [s for s in sensor_list if s.in_any_group(self.config.metric_groups)]
+        self.mqtt_client = plugin_context.mqtt_client
+        sensor_configs: List[Tuple[str, str]] = list(map(self.__configure_sensor, self.sensors))
+        self.publisher = MqttAutoConfPublisher(self.config, self.mqtt_client, sensor_configs)
+
 
     def get_event_processors(self) -> [DeyeEventProcessor]:
-        """We dont need to process events, so we return an empty list 
+        """Provides a list of custom event processors 
         """
-        return []
+        return [self.publisher]
     
-    def __configure_sensors(self, sensor: SingleRegisterSensor) -> [DeyeEventProcessor]:
+    def __configure_sensor(self, sensor: Sensor) -> Tuple[str, str]:
         """Create a Home Assistant MQTT auto discovery configuration for a sensor 
         """
         unique_id = f"deye_{self.config.logger.serial_number}_{sensor.mqtt_topic_suffix.replace('/', '' )}"
         mqtt_topic = f"homeassistant/sensor/{unique_id}/config"
         ha_config = {
             'device_class': f"{sensor.device_class}",
+            'state_class': sensor.state_class if sensor.state_class is not None else 'measurement',
             'name': f"{sensor.name}",
             'state_topic': f"{self.config.mqtt.topic_prefix}/{sensor.mqtt_topic_suffix}",
             'unit_of_measurement': f"{sensor.unit}",
             'unique_id': f"{unique_id}",
+            'expire_after': 300,
             'device': {
                 'identifiers': [f"deye{self.config.logger.serial_number}"],
                 'name': f"Deye {self.config.logger.serial_number}"
             }
         }
-        self.mqtt_client.publish_sensor(topic=mqtt_topic, value=json.dumps(ha_config))
-        return self.sensors
+        return (mqtt_topic, json.dumps(ha_config))
+        
--- src/deye_sensor.py
+++ src/deye_sensor.py
@@ -25,12 +25,22 @@ class Sensor:
     This is an abstract class. Method 'read_value' must be provided by the extending subclass.
     """
 
-    def __init__(self, name: str, mqtt_topic_suffix="", unit="", print_format="{:s}", device_class='', groups=[]):
+    def __init__(
+        self,
+        name: str,
+        mqtt_topic_suffix="",
+        unit="",
+        print_format="{:s}",
+        device_class="",
+        state_class=None,
+        groups=[],
+    ):
         self.name = name
         self.mqtt_topic_suffix = mqtt_topic_suffix
         self.unit = unit
         self.print_format = print_format
         self.device_class = device_class
+        self.state_class = state_class
         assert len(groups) > 0, f"Sensor {name} must belong to at least one group"
         self.groups = groups
 
@@ -81,10 +91,11 @@ class SingleRegisterSensor(Sensor):
         mqtt_topic_suffix="",
         unit="",
         print_format="{:0.1f}",
-        device_class='',
+        device_class="",
+        state_class=None,
         groups=[],
     ):
-        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, groups)
+        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, state_class, groups)
         self.reg_address = reg_address
         self.factor = factor
         self.offset = offset
@@ -121,10 +132,11 @@ class DoubleRegisterSensor(Sensor):
         mqtt_topic_suffix="",
         unit="",
         print_format="{:0.1f}",
-        device_class='',
+        device_class="",
+        state_class=None,
         groups=[],
     ):
-        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, groups)
+        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, state_class, groups)
         self.reg_address = reg_address
         self.factor = factor
         self.offset = offset
@@ -198,10 +210,11 @@ class ComputedPowerSensor(Sensor):
         mqtt_topic_suffix="",
         unit="",
         print_format="{:0.1f}",
-        device_class='',
+        device_class="",
+        state_class=None,
         groups=[],
     ):
-        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, groups)
+        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, state_class, groups)
         self.voltage_sensor = voltage_sensor
         self.current_sensor = current_sensor
 
@@ -224,11 +237,17 @@ class ComputedSumSensor(Sensor):
     """
 
     def __init__(
-        self, name: str, sensors: list[Sensor], mqtt_topic_suffix="", unit="",
-        print_format="{:0.1f}", device_class='', groups=[]
+        self,
+        name: str,
+        sensors: list[Sensor],
+        mqtt_topic_suffix="",
+        unit="",
+        print_format="{:0.1f}",
+        device_class="",
+        state_class=None,
+        groups=[],
     ):
-        super().__init__(name, mqtt_topic_suffix, unit, print_format,
-                         device_class, groups)
+        super().__init__(name, mqtt_topic_suffix, unit, print_format, device_class, state_class, groups)
         self.sensors = sensors
 
     def read_value(self, registers: dict[int, bytearray]):
